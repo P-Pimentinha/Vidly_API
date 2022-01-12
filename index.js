759f8da734261896ec2b3c6920f92c811a3d3d44
Pedro Pimentinha <pedro.pimentinha18@gmail.com>
2022-01-12 20:29:02 +0100

    added get endpoint that will return an object based on ID

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ app.get('/api/movies/', (req,res) => {
     res.send(movies);
 });
 
+app.get('/api/movies/:id', (req,res) => {
+    const movie = movies.find(c => c.id === parseInt(req.params.id));
+    if(!movie) res.status(404).send('The movie with the give ID was not found');
+    res.send(movie);
+});
+
 
 
 
