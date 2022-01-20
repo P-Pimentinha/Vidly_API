978936f0b24b7d78717e7dc7f13161bbf8591c69
Pedro Pimentinha <pedro.pimentinha18@gmail.com>
2022-01-20 18:14:17 +0100

    added a put router

--- customers/customers.js
+++ customers/customers.js
@@ -6,20 +6,23 @@ const mongoose = require('mongoose');
 
 
 const Customer = mongoose.model('Customer', new mongoose.Schema({
-    isGold: Boolean,
-    name:{
-        type: String,
-        required: true,
-        minlength: 2,
-        maxlength: 50
-      },
-    phoneNumber:{
-        type: String,
-        required: true,
-        minlength: 2,
-        maxlength: 50
-      },
-}))
+    name: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 50
+    },
+    isGold: {
+      type: Boolean,
+      default: false
+    },
+    phone: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 50
+    }
+  }));
 
 router.get('/', async (req, res) => {
     const customers = await Customer.find();
@@ -30,16 +33,40 @@ router.get('/', async (req, res) => {
     const { error } = validateCustomer(req.body); 
     if (error) return res.status(400).send(error.details[0].message);
   
-    let customer = new Customer({ name: req.body.name });
+    let customer = new Customer({ 
+      name: req.body.name,
+      isGold: req.body.isGold,
+      phone: req.body.phone
+    });
     customer = await customer.save();
     
     res.send(customer);
   });
 
+  router.put('/:id', async (req, res) => {
+    const { error } = validateCustomer(req.body); 
+    if (error) return res.status(400).send(error.details[0].message);
+  
+    const customer = await Customer.findByIdAndUpdate(req.params.id, { 
+        name: req.body.name,
+        isGold: req.body.isGold,
+        phone: req.body.phone }, {
+      new: true
+    });
+  
+    if (!customer) return res.status(404).send('The customer with the given ID was not found.');
+    
+    res.send(customer);
+  });
+
+
+
 
   function validateCustomer(customer) {
     const schema = {
-      name: Joi.string().min(3).required()
+      name: Joi.string().min(5).max(50).required(),
+      phone: Joi.string().min(5).max(50).required(),
+      isGold: Joi.boolean()
     };
   
     return Joi.validate(customer, schema);
